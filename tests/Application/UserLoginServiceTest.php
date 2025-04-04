781f69be43ba9dca666ac271a152fd4acfefb60f
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2025-04-04 12:54:57 +0200

    [1] - Primer test creado (userAlreadyLoggedIn)

--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -6,16 +6,28 @@ namespace UserLoginService\Tests\Application;
 
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+    public function userAlreadyLoggedIn(): void
     {
+        $user = new User('Asier');
         $userLoginService = new UserLoginService();
-
-        $this->assertEquals("user logged", $userLoginService->manualLogin());
+        $this->expectExceptionMessage("User already logged in");
+        $userLoginService ->manualLogin($user);
     }
+
+    /**
+     * @test
+     */
+    public function userIsLoggedIn(): void
+    {
+
+    }
+
+
 }
